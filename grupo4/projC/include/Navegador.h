50dfb27009dcb730a89d09b60255188955de0fc9
Duarte Vilar <48862635+mishlawi@users.noreply.github.com>
2020-04-03 17:02:37 +0100

    *** correcao erro

---
+++ grupo4/projC/include/Navegador.h
@@ -0,0 +1,24 @@
+#ifndef Navegador_H
+#define Navegador_H
+
+#include <glib.h>
+#include <stdio.h>
+
+#define MaxElem 20
+
+typedef struct lst *Lista;
+typedef struct pagina *Pagina;
+
+Lista inicializa_lista();
+void removeLst(Lista lst);
+void insereLista(gpointer lst,gpointer value);
+Pagina initPagina();
+Pagina getPageSeguinte(Lista lst,int sp);
+int paginasTotal(Lista lst);
+char* getString(Pagina pg,int index);
+int sizeLst(Lista lst);
+int sizeLstPage(Pagina p);
+char* getStringLst(Lista lst,int index);
+
+
+#endif
