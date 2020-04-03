a921e6bbd175a90d19fd9cab1ecec028e48d3dfc
I-Mic <61886573+I-Mic@users.noreply.github.com>
2020-04-03 17:03:12 +0100

    Add files via upload

---
+++ grupo4/projC/include/Files.h
@@ -0,0 +1,19 @@
+#ifndef Files_H
+#define Files_H
+
+#include <stdlib.h>
+#include <glib.h>
+#include "CatProdutos.h"
+#include "CatClientes.h"
+#include "Produto.h"
+#include "Cliente.h"
+#include "Venda.h"
+#include "Faturacao.h"
+#include "Filial.h"
+
+void loadFilesProduto(char* file,Cat_Produtos cp);
+void loadFilesCliente(char* file,Cat_Clientes cc);
+void leVendas(Filial fil[3],Faturacao f,char* file,char* arr_token[],Cat_Produtos cp,Cat_Clientes cc);
+
+
+#endif
