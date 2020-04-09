365cc29026bf894b6fa2ba9cad6c0ebc21f0b059
Duarte Vilar <48862635+mishlawi@users.noreply.github.com>
2020-04-09 18:24:49 +0100

    Documentação

--- grupo4/projC/src/CatClientes.c
+++ grupo4/projC/src/CatClientes.c
@@ -1,14 +1,16 @@
 #include "../include/CatClientes.h"
 
+//Estrutura de dados do catalogo de clientes
 struct cat_clientes{
 	GTree* clientes[Clientes];
 };
 
+//Elimina a chave de um cliente
 void freeKeyClient(gpointer data){
 	char* clienteID=(char*)data;
 	free(clienteID);
 }
-
+//Alocação de memoria da estrutura de dados para sua inicialização
 Cat_Clientes inicializa_CatClientes(){
 	Cat_Clientes ct=(Cat_Clientes)malloc(sizeof(struct cat_clientes));
 	for (int i=0;i<Clientes;i++)
@@ -16,14 +18,14 @@ Cat_Clientes inicializa_CatClientes(){
 	return ct;
 }
 
-
+//Insere um cliente no catalogo de clientes
 Cat_Clientes insereCliente(Cat_Clientes cc,Cliente c){
 	int i=getIndexC(c);
 	char* code=getCliente(c);
  	g_tree_insert(cc->clientes[i],code,code);
  	return cc;
 }
-
+//Verifica a existencia de um cliente no catalogo de clientes
 gboolean existeCliente(Cat_Clientes cp, Cliente c){
 	int i=getIndexC(c);
 	char* code=getCliente(c);
@@ -31,20 +33,20 @@ gboolean existeCliente(Cat_Clientes cp, Cliente c){
  	free(code);	
  	return FALSE;
 }
-
+//Determina o numero total de clientes existentes no catalogo
 int totalClientes(Cat_Clientes cc){
 	int total=0;
  	for(int i=0;i<26;i++)
   		total+=g_tree_nnodes(cc->clientes[i]);
  	return total;
 }
-
+//Desalocação de memória da estrutura de dados
 void removeCatCliente(Cat_Clientes cc){
  	for(int i=0;i<Clientes;i++)
   		g_tree_destroy(cc->clientes[i]);  
  	free(cc);
 }
-
+//Retorna a árvore correspondente a um determinado cliente a partir da sua chave
 GTree* getTreeC(Cat_Clientes cc,int index){
 	return cc->clientes[index];
 }
