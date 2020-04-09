c1819d9f775f40ef1d178abfb7094d48c46e37d4
Duarte Vilar <48862635+mishlawi@users.noreply.github.com>
2020-04-09 18:59:43 +0100

    Documentação

--- grupo4/projC/src/Cliente.c
+++ grupo4/projC/src/Cliente.c
@@ -1,28 +1,30 @@
 #include "../include/Cliente.h"
-
+//Estrutura de um cliente
 struct cliente{
 	char* codigo;
 };
 
+//Alocação de memoria de um cliente
 Cliente inicializa_Cliente(){
  	Cliente c=(Cliente)malloc(sizeof(struct cliente));
  	c->codigo=NULL;
  	return c;
 }
-
+//Criação/definição um cliente
 Cliente criaCliente(Cliente c,char* codigo){
  	c->codigo=strdup(codigo);
  	return c;
 }
-
+//Obter codigo de um cliente
 char* getCliente(Cliente c){
  	return strdup(c->codigo);
 }
-
+//Determinar index ao qual cliente irá pertencer no CatClientes
 int getIndexC(Cliente c){
  	return (c->codigo[0]-'A');
 }
 
+//Verifica se um codigo de cliente é valido tendo em conta os parâmetros
 gboolean validaCliente(Cliente c){
   	if (!isalpha(c->codigo[0])) return FALSE;
   	if (!(c->codigo[0] >='A' && c->codigo[0]<='Z')) return FALSE;
@@ -30,6 +32,7 @@ gboolean validaCliente(Cliente c){
  	return TRUE;
 }
 
+//Desalocação na memória de um cliente
 void removeCliente(Cliente c){
  	
  	free(c->codigo);
