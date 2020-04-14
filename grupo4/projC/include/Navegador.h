6e495e2cf8a75267900c9d45820200179f75a26a
mishlawi <duartevilar99@gmail.com>
2020-04-15 00:07:17 +0100

    documentação

--- grupo4/projC/include/Navegador.h
+++ grupo4/projC/include/Navegador.h
@@ -9,17 +9,102 @@ typedef struct lst *Lista;
 typedef struct pagina *Pagina;
 
 Lista inicializa_lista();
-void removeLst(Lista lst);
-void insereLista(gpointer lst,gpointer value);
-Pagina initPagina();
-Pagina getPageSeguinte(Lista lst,int sp);
-int paginasTotal(Lista lst);
-char* getString(Pagina pg,int index);
-int sizeLst(Lista lst);
-int sizeLstPage(Pagina p);
-char* getStringLst(Lista lst,int index);
-void removePagina(Pagina p);
+/* @brief Aloca memória para a criação de uma estrutura Lista.
+ *
+ *  @param 
+ *
+ *  @return Lista.
+ */
 
+
+void removeLst(Lista lst);
+/* @brief Remove a estrutura Lista, libertando memória.
+ *
+ *  @param Lista a ser removida
+ *
+ *  @return void.
+ */
+
+
+void insereLista(gpointer lst,gpointer value);
+/* @brief Inser um elemento (value) na lista.
+ *
+ *  @param gpointer da lista onde o valor é inserido, gpointer do valor a ser inserido
+ *
+ *  @return void.
+ */
+
+
+Pagina initPagina();
+/* @brief Aloca memória para a criação de uma estrutura Pagina.
+ *
+ *  @param 
+ *
+ *  @return Pagina.
+ */
+
+
+Pagina getPageSeguinte(Lista lst,int sp);
+/* @brief Retorna uma determinada pagina.
+ *
+ *  @param Lista, int com o numero da pagina
+ *
+ *  @return Pagina.
+ */
+
+
+int paginasTotal(Lista lst);
+/* @brief Determina o numero total de paginas .
+ *
+ *  @param Lista
+ *
+ *  @return int.
+ */
+
+
+char* getString(Pagina pg,int index);
+/* @brief Retorna a String de um index de uma pagina.
+ *
+ *  @param Pagina, int com o index da pagina
+ *
+ *  @return char*.
+ */
+
+
+int sizeLst(Lista lst);
+/* @brief Indica o tamanho de uma lista.
+ *
+ *  @param Lista
+ *
+ *  @return int.
+ */
+
+
+int sizeLstPage(Pagina p);
+/* @brief Retorna o tamanho de uma pagina.
+ *
+ *  @param Pagina
+ *
+ *  @return int.
+ */
+
+
+char* getStringLst(Lista lst,int index);
+/* @brief Retorna a String de um indice de uma lista.
+ *
+ *  @param Lista, int com o indice da lista
+ *
+ *  @return char*.
+ */
+
+
+void removePagina(Pagina p);
+/* @brief Remove a estrutura Pagina, libertando memória.
+ *
+ *  @param Pagina
+ *
+ *  @return void.
+ */
 
 
 #endif
--- grupo4/projC/include/interface.h
+++ grupo4/projC/include/interface.h
@@ -15,19 +15,130 @@
 typedef struct sgv *SGV;
 
 SGV initSGV();
+
 void destroySGV(SGV sgv);
+/* @brief Remove a estrutura SGV, libertando memória.
+ *
+ *  @param 
+ *
+ *  @return void.
+ */
+
+
 SGV loadFromFiles(char* filepath[3],SGV sgv);
+/* @brief Faz a leitura dos ficheiros pré-definidos ou personalizados.
+ *
+ *  @param char** com os paths dos ficheiros, estrutura SGV
+ *
+ *  @return SGV.
+ */
+
+
 Lista getProductsStartedByLetter(SGV sgv,char letter);
+/* @brief Apresenta a lista de produtos começados por uma determinada letra
+ *
+ *  @param estrutura SGV, char com a letra em questão
+ *
+ *  @return Lista.
+ */
+
+
 Lista getProductSalesAndProfit(SGV sgv,char* productID,int month);
+/* @brief Apresenta a lista de registos e do total de vendas de um determinado produto, num determinado mês 
+ *
+ *  @param estrutura SGV, char* com o codigo de produto, int que representa o mês
+ *
+ *  @return Lista.
+ */
+
+
 Lista getProductsNeverBought(SGV,int branchID);
+/* @brief Lista ordenada de códigos de produtos que ninguém comprou (Total ou por filial).
+ *
+ *  @param estrutura SGV, int com o numero da filial 
+ *
+ *  @return Lista.
+ */
+
+
 Lista getClientesOfAllBranches(SGV sgv);
+/* @brief Lista de códigos de clientes que realizaram compras em todas as filiais.
+ *
+ *  @param estrutura SGV
+ *
+ *  @return Lista.
+ */
+
+
 Par getClientsAndProductsNeverBoughtCount(SGV sgv);
+/* @brief Clientes que não realizaram compras e número de produtos nunca comprados.
+ *
+ *  @param estrutura SGV
+ *
+ *  @return Par.
+ */
+
+
 Tabela getProductBoughtByClient(SGV sgv,char* clientID);
+/* @brief Tabela de produtos comprados por mês e filial.
+ *
+ *  @param estrutura SGV, codigo cliente
+ *
+ *  @return Tabela.
+ */
+
+
 Profit getSalesAndProfit(SGV sgv,int minMonth,int maxMonth);
+/* @brief Determinar total de vendas num determinado intervalo.
+ *
+ *  @param estrutura SGV, int com o mês inicial, int com o mês final
+ *
+ *  @return void.
+ */
+
+
 LstBuyers getProductBuyers(SGV sgv,char* productID,int branchID);
+/* @brief Determinar os códigos e o número total dos clientes que compraram um produto numa filial.
+ *
+ *  @param estrutura SGV, int com o codigo de produto , int com a filial
+ *
+ *  @return LstBuyers.
+ */
+
+
 Lista getClientFavoriteProducts(SGV sgv,char* clientID,int month);
+/* @brief Determinar a lista de produtos que um cliente mais comprou por quantidade, num determinado mês.
+ *
+ *  @param estrutura SGV, char* com o codigo de cliente, int com o mes
+ *
+ *  @return Lista.
+ */
+
+
 SelledProd* getTopSelledProducts(SGV sgv,int limit);
+/* @brief Top personalizado de vendas,por número de cliente e quantidade vendida, filial a filial.
+ *
+ *  @param estrutura SGV, int com o limite do top
+ *
+ *  @return SelledProd*.
+ */
+
+
 GList* getClientTopProfitProducts(SGV sgv,char* clientID,int limit);
+/* @brief Top personalizado de gasto em compras individualizado por cliente.
+ *
+ *  @param estrutura SGV, char* com o codigo de cliente, int com o limite do top
+ *
+ *  @return GList*.
+ */
+
+
 FileInfo getCurrentFilesInfo(SGV sgv);
+/* @brief Informação dos ficheiros lidos.
+ *
+ *  @param estrutura SGV
+ *
+ *  @return FileInfo.
+ */
 
 #endif
